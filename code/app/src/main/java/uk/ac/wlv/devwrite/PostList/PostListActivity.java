95afcfc03b2b4c77a53e984d2d24a93f0b7e4dea
Christa Bridges <christa@pebblepad.co.uk>
2023-04-22 11:15:07 +0100

    Creating fragment for post list

--- code/app/src/main/java/uk/ac/wlv/devwrite/PostList/PostListActivity.java
+++ code/app/src/main/java/uk/ac/wlv/devwrite/PostList/PostListActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import uk.ac.wlv.devwrite.R;
 
@@ -12,6 +14,12 @@ public class PostListActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_activity_post);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
 
+        if (fragment == null) {
+            fragment = new PostListFragment();
+            fragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
