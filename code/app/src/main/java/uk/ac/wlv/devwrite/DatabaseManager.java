3625a8c57724bb64d5080edd38f5efca6323236d
Christa Bridges <christa@pebblepad.co.uk>
2023-04-22 11:49:50 +0100

    Adding DatabaseManager with functionality to get all the posts

---
+++ code/app/src/main/java/uk/ac/wlv/devwrite/DatabaseManager.java
@@ -0,0 +1,65 @@
+package uk.ac.wlv.devwrite;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.CursorWrapper;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import uk.ac.wlv.devwrite.Models.Post;
+import uk.ac.wlv.devwrite.database.PostBaseHelper;
+import uk.ac.wlv.devwrite.database.PostCursorWrapper;
+import uk.ac.wlv.devwrite.database.PostDbSchema;
+
+public class DatabaseManager {
+    private static DatabaseManager sDatabaseManager;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
+    public static DatabaseManager get(Context context) {
+        if (sDatabaseManager == null) {
+            sDatabaseManager = new DatabaseManager(context);
+        }
+
+        return sDatabaseManager;
+    }
+
+    private DatabaseManager(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new PostBaseHelper(mContext).getWritableDatabase();
+    }
+
+    public List<Post> getPosts() {
+        List<Post> posts = new ArrayList<>();
+        PostCursorWrapper cursor = queryPosts(null, null);
+
+        try {
+            cursor.moveToFirst();
+
+            while (!cursor.isAfterLast()) {
+                posts.add(cursor.getPost());
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
+
+        return posts;
+    }
+
+    private PostCursorWrapper queryPosts(String whereClause, String[] whereArgs) {
+        Cursor cursor = mDatabase.query(
+                PostDbSchema.PostTable.NAME,
+                null,
+                whereClause,
+                whereArgs,
+                null,
+                null,
+                null
+        );
+
+        return new PostCursorWrapper(cursor);
+    }
+}
